b5e0fab9a399f040f6cefb2a197b83055ad618a4
Leo78923 <117394079+Leo78923@users.noreply.github.com>
2023-10-19 17:17:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 video = "";
 modelStatus = "";
+objects = [];
+
 
 function preload() 
 {
@@ -16,6 +18,33 @@ function setup()
 function draw()
 {
     image(video, 0, 0, 480, 380);
+
+    if(modelStatus != "")
+    {
+        objectDetector.detect(video, gotResult);
+        for(i = 0; i < objects.length;i++)
+        {
+            document.getElementById("status").innerHTML = "Status: Detected Objects";   
+            document.getElementById("numberOfObjects").innerHTML = "Number of Detected Objects: "+ objects.length;   
+            
+            fill("#4D07B1");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#4D07B1");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }   
+    }
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
 
 function start() 
